132c09c961feddf4a078dbd247c62b1a087821f0
ysyed <ysyed@pcienergysolutions.com>
2022-10-13 21:32:59 -0500

    completed 3rd module

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -16,6 +16,7 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+	@Version private Long version;
     private String firstname;
     private String lastname;
 	@JsonIgnore
